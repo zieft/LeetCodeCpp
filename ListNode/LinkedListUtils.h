bf443e9ab54dd12a834a611cdbf36e9b240858f7
Yulin Zhu <Y.Zhu@wzl.rwth-aachen.de>
2023-05-16 18:52:32 +0200

    21题跟python一样的交换节点并递归的方法。

--- ListNode/LinkedListUtils.h
+++ ListNode/LinkedListUtils.h
@@ -8,15 +8,22 @@
 #include "ListNode.h" // 包含ListNode的定义
 
 using namespace std;
-//ListNode* create_linklist_tail(vector<int>& li);
-//class LinkedListUtils {
-//
-//};
+
 class BaseSolution {
 public:
-    ListNode* create_linklist_tail(vector<int>& li);
-    ListNode* createLinkedList(const std::vector<int>& arr);
+    ListNode* create_linklist_tail(vector<int>& li);  // 尾插法
+    ListNode* createLinkedList(const std::vector<int>& arr); // 头插法
     void printLinkedList(ListNode* head);
 };
 
 #endif //LEETCODE_LINKEDLISTUTILS_H
+
+/*
+为什么成员函数要用指针的方式定义?
+
+成员函数使用指针方式定义的主要原因是在函数内部修改链表结构时，需要修改链表节点的指针，而不仅仅是修改函数内部的局部变量。
+通过使用指针方式定义成员函数，可以直接操作链表节点的指针，对链表进行修改。
+
+如果成员函数不使用指针方式定义，而是传递链表对象或引用，那么在函数内部对链表进行修改时，只会修改函数内部的局部副本，不会对原始链表产生影响。
+因此，为了在成员函数中对链表进行修改，需要使用指针方式定义，以便直接修改链表节点的指针。
+*/
